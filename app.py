77207c36ae6998d9ff0b5b93ed552339f030ec68
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2022-02-26 04:28:13 +0530

    Add model.py to contain main logic of generating calendar logic and use in app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, jsonify
+from model import getDateMatrix
+from exceptions import InvalidDateFormat
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def home(date: str):  # Format: "YYYY-MM-DD"
-    pass
+    """ Home route of the flask application
+    Parameters:
+        date: str
+            Date for which calendar is required
+    Returns:
+        dateMatrix: tuple(json, int)
+            Returns the tuple of json response and status code
+    """
+    try:
+        dateMatrix = getDateMatrix(date)
+        return jsonify(dateMatrix), 200
+    except InvalidDateFormat:
+        return jsonify("Invalid date format passed. Accepted format: YYYY-MM-DD"), 400
+    except:
+        return jsonify("Server side issue"), 500
 
 
 if __name__ == "__main__":
---
+++ model.py
@@ -0,0 +1,13 @@
+_PIVOT_DATE = "1752-09-14"
+_PIVOT_DAY = ""
+
+def getDateMatrix(date: str):  # Format: "YYYY-MM-DD"
+    """ Computes the date matrix for a given date
+    Parameters:
+        date: str
+            Date for which calendar is required
+    Returns:
+        dateMatrix: tuple(json, int)
+            Returns the tuple of json response and status code
+    """
+    pass
