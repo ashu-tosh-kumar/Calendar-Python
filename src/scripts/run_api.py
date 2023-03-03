d439231b4a1a946cbe2530a63610a6954d96533c
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2023-03-04 03:47:40 +0530

    fix bug

--- src/scripts/run_api.py
+++ src/scripts/run_api.py
@@ -8,8 +8,5 @@ if config.ENV in [
     print("Starting the application server")
     os.system("gunicorn --workers=4 --bind=0.0.0.0:8000 src.app:app")
 else:
-    print("Setting up Cassandra")
-    os.system("python src/scripts/run_local_db_setup.py")
-
     print("Starting the application server")
     os.system("gunicorn --workers=4 --bind=0.0.0.0:8000 src.app:app --reload")
